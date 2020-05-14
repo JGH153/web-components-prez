2dde352ec81155ac29f8e23c8844d0147df9bada
JGH153 <jan.greger@gmail.com>
2020-05-14 11:54:22 +0200

    Some content of prez

---
+++ demo/examples/Custom-element.js
@@ -0,0 +1,38 @@
+'use strict';
+class MyElement extends HTMLElement {
+
+	constructor() {
+    // Always call super first in constructor
+    super();
+    console.log('CardComponent constructor')
+
+    this.setupShadow();
+    // this.addCssDynamic();
+	}
+	
+	connectedCallback() {
+		console.log('connectedCallback');
+	}
+
+	disconnectedCallback() {
+    console.log('connectedCallback');
+  }
+
+  adoptedCallback() {
+    console.log('adoptedCallback');
+	}
+	
+	attributeChangedCallback(name, oldValue, newValue) {
+		console.log('attributeChangedCallback');
+	}
+
+	setupShadow() {
+    this.shadow = this.attachShadow({ mode: 'open' });
+    const template = document.getElementById('html-template');
+    const templateContent = template.content;
+    const shadowRoot = this.shadow.appendChild(templateContent.cloneNode(true));
+  }
+
+}
+
+customElements.define('my-element', MyElement);
